7f2cefd9a92bf00920d05f204f3b9f2b006c39c0
KevinX8 <pgnmaster24@gmail.com>
2020-12-11 00:46:20 +0000

    Updates

---
+++ precache-manifest.07776992dc729723cc22501bca6a4cb6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d76c1b49ab86ddb119d7d4aa88535c6",
+    "url": "/vanced-website-v2/index.html"
+  },
+  {
+    "revision": "3210e199ff444be44d61",
+    "url": "/vanced-website-v2/static/css/main.d730ea7c.chunk.css"
+  },
+  {
+    "revision": "6c33c874fb69b1163aa9",
+    "url": "/vanced-website-v2/static/js/2.a1b9f948.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/vanced-website-v2/static/js/2.a1b9f948.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3210e199ff444be44d61",
+    "url": "/vanced-website-v2/static/js/main.cfa88210.chunk.js"
+  },
+  {
+    "revision": "0c87cd0af3f6c9cb8ddd",
+    "url": "/vanced-website-v2/static/js/runtime-main.5a0169aa.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/vanced-website-v2/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "1237a2632e7396ce2d221a3db1f1e186",
+    "url": "/vanced-website-v2/static/media/customization_1.1237a263.jpg"
+  },
+  {
+    "revision": "ee67e587228bded22a17777adde2d129",
+    "url": "/vanced-website-v2/static/media/customization_2.ee67e587.jpg"
+  },
+  {
+    "revision": "704f1f846ad4eba6525432dca978e4b1",
+    "url": "/vanced-website-v2/static/media/download.704f1f84.svg"
+  },
+  {
+    "revision": "865ba77420af826c8672659f13882103",
+    "url": "/vanced-website-v2/static/media/illustration-section-01.865ba774.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/vanced-website-v2/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "90a40cae97d18f360923b59d56e5dcf1",
+    "url": "/vanced-website-v2/static/media/logo.90a40cae.svg"
+  },
+  {
+    "revision": "653a3d5bdce0a1cbb5ae6915a87bdaa2",
+    "url": "/vanced-website-v2/static/media/main_features_1.653a3d5b.jpg"
+  },
+  {
+    "revision": "315d3bb29b6e1c0d3c2ac0f7cd3db277",
+    "url": "/vanced-website-v2/static/media/sponsor_block_1.315d3bb2.jpg"
+  },
+  {
+    "revision": "02c16764ff0cf4a5bfe6672641a4f365",
+    "url": "/vanced-website-v2/static/media/sponsor_block_2.02c16764.jpg"
+  }
+]);
